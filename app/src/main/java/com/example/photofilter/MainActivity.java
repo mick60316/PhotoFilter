edd270499f81103bc4861e3ac3cc01212ad141c4
Mike Notebook <Mick60316@gmail.com>
2020-08-20 15:19:43 +0800

    Photo  Filter Init

---
+++ app/src/main/java/com/example/photofilter/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.photofilter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.opencv.android.OpenCVLoader;
+import org.opencv.videoio.VideoCapture;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
